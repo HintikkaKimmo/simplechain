fc56d00f655619940359fcc5d9145bb00057f082
Kimmo Hintikka <hintikkakimmo@gmail.com>
2018-01-25 11:13:50 +0000

    initial commit for simple blockchain

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+// Block type defination
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+// Blockchain type defination. In practice array of pointers to Blocks
+type Blockchain struct {
+	blocks []*Block
+}
+
+// SetHash method calculates the hash value
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+
+}
+
+// AddBlock adds blocks to the chain
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+// NewBlock functions creates new chain block
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+// NewGenesisBlock function creates genesis block to be used to start the chain
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+// NewBlockChain uses NewGenesisBlock Block to create the initial chain
+func NewBlockChain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func main() {
+	bc := NewBlockChain()
+
+	bc.AddBlock("Sent 1 token to Bob")
+	bc.AddBlock("Sent 2 more tokens to Tim")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
